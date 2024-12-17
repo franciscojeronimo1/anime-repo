7eaf7edc33ab946f870014762c3fe57711790963
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2024-12-17 12:31:02 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  state = {
+    name: 'Dragaon ball',
+    idade: 15,
+    Protagonista: 'Goku'
+  }
 
-    </div>
-  );
+  click = () => {
+    this.setState({ name: 'Junior' })
+  }
+
+  render() {
+    const { name } = this.state
+    const { idade } = this.state
+    const { Protagonista } = this.state
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.click}>
+            nome do anime: {name}
+          </p> <br />
+          <p>idade do anime: {idade}</p><br />
+          <p>Protagonista: {Protagonista}</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+
+      </div>
+    );
+  }
 }
 
 export default App;
